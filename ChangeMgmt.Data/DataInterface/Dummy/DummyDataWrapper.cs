2785d48815981961ef48588c59c51e2bfeb2b29d
Tim Hall <email>
2019-01-02 18:51:27 +0000

    Adding dummy data wrapper class

---
+++ ChangeMgmt.Data/DataInterface/Dummy/DummyDataWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ChangeMgmt.Data.DataInterface.Dummy
+{
+    public class DummyDataWrapper : IDataWrapper
+    {
+        public DummyDataWrapper()
+        {
+        }
+
+        public bool IsPasswordValid(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPasswordValid(string email, byte[] password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
