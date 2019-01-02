418649586c723f379abc063d720c22e9ccded79b
Tim Hall <email>
2019-01-02 19:19:50 +0000

    Adding cryptography class

--- ChangeMgmt.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ChangeMgmt.Common
-{
-    public class Class1
-    {
-    }
-}
---
+++ ChangeMgmt.Common/Cryptography.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ChangeMgmt.Common
+{
+    public class Cryptography
+    {
+        public Cryptography()
+        {
+        }
+
+        public byte[] SHA265(string text)
+        {
+            return new byte[32];
+        }
+    }
+}
