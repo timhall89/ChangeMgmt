fbbaa81ab91afa6ef52dd26f8392fb5655ba691a
Tim Hall <email>
2019-01-02 19:45:49 +0000

    removing Conn String

--- ChangeMgmt.Data/Models/SqlServerDB/ChangeMgmtContext.cs
+++ ChangeMgmt.Data/Models/SqlServerDB/ChangeMgmtContext.cs
@@ -19,11 +19,7 @@ namespace ChangeMgmt.Data.Models.SqlServerDB.Entities
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=ChangeMgmt;User Id=sa;Password=D0ck3r5QL53rv3r;");
-            }
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
