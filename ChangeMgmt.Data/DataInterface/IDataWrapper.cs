a385a0329333000046df574b27d9f18a4907b1fc
Tim Hall <email>
2019-01-02 18:58:49 +0000

    Changes to IDataWrapper

--- ChangeMgmt.Data/DataInterface/IDataWrapper.cs
+++ ChangeMgmt.Data/DataInterface/IDataWrapper.cs
@@ -3,6 +3,7 @@ namespace ChangeMgmt.Data.DataInterface
 {
     public interface IDataWrapper
     {
-
+        bool IsPasswordValid(string email, string password);
+        bool IsPasswordValid(string email, byte[] password);
     }
 }
