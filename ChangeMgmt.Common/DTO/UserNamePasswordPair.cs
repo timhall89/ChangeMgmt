0e66635915702ce10a927669e854c8c40be6b19b
Tim Hall <thall@adgcorporate.co.uk>
2019-01-04 09:25:17 +0000

    adding first DTO

---
+++ ChangeMgmt.Common/DTO/UserNamePasswordPair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeMgmt.Common.DTO
+{
+    public class PasswordValidationData
+    { 
+        public PasswordValidationData(string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+        public string UserName { get; }
+        public string Password { get; }
+    }
+}
