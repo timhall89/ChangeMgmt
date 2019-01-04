f46dfe4191b8ecc02ad231dbe452d22ff206e003
Tim Hall <thall@adgcorporate.co.uk>
2019-01-04 09:24:40 +0000

    Adding login helper method for validating password

---
+++ ChangeMgmt.BusinessLogic/Helpers/LoginHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ChangeMgmt.BusinessLogic.Helpers
+{
+    using Common.DTO;
+    public class LoginHelper
+    {
+        public bool IsUserPasswordCorrect(IEnumerable<UserNamePasswordPair> DataSet, UserNamePasswordPair User)
+        {
+            List<string> PasswordsForUserName = DataSet.Where(DataSetUser => DataSetUser.UserName == User.UserName)
+                .Select(DataSetUser => DataSetUser.Password)
+                .ToList();
+
+            if (!PasswordsForUserName.Any()) throw new ArgumentException($"No user exists with user name {User.UserName}");
+
+            return PasswordsForUserName.Any(pw => pw == User.Password);
+        }
+    }
+}
