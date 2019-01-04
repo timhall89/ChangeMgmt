92e147d1a6f5cb347f6e853bafc55fb5231e4af1
Tim Hall <thall@adgcorporate.co.uk>
2019-01-04 09:56:17 +0000

    Changing DTO to use interfaces

---
+++ ChangeMgmt.Common/DTO/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeMgmt.Common.DTO
+{
+    using DTOInterfaces;
+    public class User : IUserNameAndPassword                
+    {
+        public User(string UserName, string Password) : this(UserName, Cryptography.GetSHA256(Password))
+        {
+
+        }
+        
+        public User(string UserName, byte[] Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+        public string UserName { get; }
+        public byte[] Password { get; }
+    }
+}
---
+++ ChangeMgmt.Common/DTOInterfaces/IUserNameAndPassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeMgmt.Common.DTOInterfaces
+{
+    public interface IUserNameAndPassword
+    {
+        string UserName { get; }
+        byte[] Password { get; }
+    }
+}
