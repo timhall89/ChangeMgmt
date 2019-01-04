1657bf55650bd3e69279856038dde95959c5c377
Tim Hall <thall@adgcorporate.co.uk>
2019-01-04 10:34:17 +0000

    Adding tests for login details checking

---
+++ ChangeMgmt.Tests/LoginTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChangeMgmt.Tests
+{
+    using BusinessLogic.Helpers;
+    using Common.DataWrapper.Wrappers;
+    using Common.DTO;
+
+    public class LoginTests
+    {
+        [Fact]
+        public void CorrectPasswordLogin()
+        {
+            User user = new User("Tim", "password1");
+
+            Assert.True(LoginHelper.IsUserPasswordCorrect(new DummyDataWrapper().UserNamesAndPasswords, user));  
+        }
+
+        [Fact]
+        public void IncorrectPasswordLogin()
+        {
+            User user = new User("Tim", "myPassword");
+
+            Assert.False(LoginHelper.IsUserPasswordCorrect(new DummyDataWrapper().UserNamesAndPasswords, user));
+        }
+
+        [Fact]
+        public void UserDoesNotExist()
+        {
+            User user = new User("Tim", "password1");
+
+            Exception ex = Assert.Throws<ArgumentException>(() => LoginHelper.IsUserPasswordCorrect(new DummyDataWrapper().UserNamesAndPasswords, user));
+            Assert.Equal($"No user exists with user name {user.UserName}", ex.Message);
+        }
+    }
+}
