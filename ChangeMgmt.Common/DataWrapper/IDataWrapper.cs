aeee77a1accb6321131b6e4c0f2b2a11f8f77b01
Tim Hall <thall@adgcorporate.co.uk>
2019-01-04 09:56:58 +0000

    Setting up data wrapping in common

---
+++ ChangeMgmt.Common/DataWrapper/IDataWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeMgmt.Common.DataWrapper
+{
+    using DTOInterfaces;
+    public interface IDataWrapper
+    {
+        IEnumerable<IUserNameAndPassword> GetUserNamesAndPasswords();
+    }
+}
---
+++ ChangeMgmt.Common/DataWrapper/Wrappers/DummyDataWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeMgmt.Common.DataWrapper.Wrappers
+{
+    using DTOInterfaces;
+    using DTO;
+    public class DummyDataWrapper : IDataWrapper
+    {
+        public IEnumerable<IUserNameAndPassword> GetUserNamesAndPasswords()
+        {
+            return new List<User> { new User("Tim", "password1")}
+        }
+    }
+}
