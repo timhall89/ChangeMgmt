06421d52d37290573dd78beb1d4d2c9d85b70248
Tim Hall <email>
2019-01-06 11:14:50 +0000

    new data wrapper interface

--- ChangeMgmt.Data/DataWrapper/IDataWrapper.cs
+++ ChangeMgmt.Data/DataWrapper/IDataWrapper.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace ChangeMgmt.Data.DataInterface
+namespace ChangeMgmt.Data.DataWrapper
 {
+    using Common.DTOInterfaces;
     public interface IDataWrapper
     {
-        bool IsPasswordValid(string email, string password);
-        bool IsPasswordValid(string email, byte[] password);
-
-        void AddUser(string Email, string FirstName, string LastName, string Password);
-        void AddUser(string Email, string FirstName, string LastName, byte[] Password);
+        bool IsPasswordValid(IUserNameAndPassword User);
     }
 }
